37e19c52b32574463519b6403ed76acb4b486ab3
jangyunhwan <jangyunhwan@gmail.com>
2021-04-25 22:18:13 +0900

    Add DOM for form tag

--- nodeJs/ejs/setDefault/index.js
+++ nodeJs/ejs/setDefault/index.js
@@ -4,6 +4,7 @@ const app = express();
 var ejs = require('ejs');
 
 app.set('view-engine','ejs');
+app.set('views','./views'); // 뷰 폴더를 ./views 경로로 설정한다.
 
 const server = app.listen(80, () => {
     console.log(' start Server :  localhost :80')
